61ceffbbcdad6c909c356218ba60278f5c15fe4c
lwm886 <227959626@qq.com>
2021-01-21 16:03:08 +0800

    enc scan

---
+++ cpt-enc/src/main/java/com/cpt/enc/core/EncScan.java
@@ -0,0 +1,32 @@
+package com.cpt.enc.core;
+
+import org.springframework.boot.context.TypeExcludeFilter;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.core.annotation.AliasFor;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+@ComponentScan(
+    excludeFilters = {@ComponentScan.Filter(
+            type = FilterType.CUSTOM,
+            classes = {TypeExcludeFilter.class}
+    )}
+)
+public @interface EncScan {
+    @AliasFor(
+            annotation = ComponentScan.class,
+            attribute = "basePackages"
+    )
+    String[] scanBasePackages() default {"com.cpt.enc"};
+
+    @AliasFor(
+            annotation = ComponentScan.class,
+            attribute = "basePackageClasses"
+    )
+    Class<?>[] scanBasePackageClasses() default {};
+}
